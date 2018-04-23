9b17564cf858ab6652507aa23efd3d76b1bb75e0
Joseph Kim <josephjdkim@gmail.com>
2018-04-23 13:23:06 -0400

    new site v3 brutalist

--- script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("show-more").addEventListener("click", expandMore);
-
-function expandMore(){
-	"use strict";
-	document.getElementById("show-more").style.display = "none";
-	document.getElementById("expanded").style.display = "block";
-}

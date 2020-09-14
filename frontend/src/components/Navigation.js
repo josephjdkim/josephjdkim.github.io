f2aa76a721e2773ef5477550090b41ead3d60c4c
Joseph Kim <josephjdkim@gmail.com>
2020-09-14 11:51:50 -0400

    styling for interactivity on site

--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -5,8 +5,8 @@ function Navigation() {
   const { contentState, setContentState } = useContext(ContentStateContext);
 
   function navClick(e) {
-    if (contentState !== e.target.className) {
-      setContentState(e.target.className);
+    if (contentState !== e.target.id) {
+      setContentState(e.target.id);
     } else {
       setContentState(null);
     }
@@ -14,8 +14,8 @@ function Navigation() {
 
   return (
     <div className="navigation">
-      <div className="nav-about" onClick={navClick}>1. About</div>
-      <div className="nav-projects" onClick={navClick}>2. Projects</div>
+      <div id="nav-about" className="nav-item" onClick={navClick}>1. About</div>
+      <div id="nav-projects" className="nav-item" onClick={navClick}>2. Projects</div>
     </div>
   );
 }
